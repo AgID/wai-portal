ac9438bedeacdb4a9ae37a5ae0d81c13b2fa66e5
Davide Porrovecchio <pdavide@gmail.com>
2018-03-28 10:02:54 +0200

    Added administratrion area menu

--- resources/views/layouts/includes/admin_menu.blade.php
+++ resources/views/layouts/includes/admin_menu.blade.php
@@ -1,4 +1,4 @@
-<li data-megamenu-class="Megamenu-item Megamenu-item--admin u-posAbsoluteRight u-zindex-min"><a href="#">Area amministrativa</a>
+<li data-megamenu-class="Megamenu-item Megamenu-item--admin u-flexExpandLeft"><a href="#">Area amministrativa</a>
   <ul>
     <li><a href="{{ route('admin-dashboard', [], false) }}">Dashboard amministrativa</a></li>
   </ul>
