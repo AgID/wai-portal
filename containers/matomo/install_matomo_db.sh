dd12d888440ae8442e649b39933f99ea147c68ad
Davide Porrovecchio <pdavide@gmail.com>
2018-03-28 18:34:46 +0200

    Fixed matomo install script

--- containers/matomo/install_matomo_db.sh
+++ containers/matomo/install_matomo_db.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-hashed_password=`php7 -r "echo password_hash(md5('@MATOMO_ROOT_PASSWORD@'), PASSWORD_BCRYPT) . PHP_EOL;"`
+hashed_password=`php7 -r "echo password_hash(md5('@MATOMO_ROOT_PASSWORD@'), PASSWORD_BCRYPT);"`
 mysql -u root -p@DB_PASSWORD@ < /opt/matomo.sql
-mysql -u root -p@DB_PASSWORD@ matomo -e "UPDATE user SET login = '@MATOMO_ROOT_USER@' WHERE superuser_access = 1;"
-mysql -u root -p@DB_PASSWORD@ matomo -e "UPDATE user SET password = '$hashed_password' WHERE superuser_access = 1;"
-mysql -u root -p@DB_PASSWORD@ matomo -e "UPDATE user SET token_auth = '@MATOMO_ROOT_APIKEY@' WHERE superuser_access = 1;"
+mysql -u root -p@DB_PASSWORD@ matomo -e 'UPDATE user SET login = "@MATOMO_ROOT_USER@" WHERE superuser_access = 1;'
+mysql -u root -p@DB_PASSWORD@ matomo -e 'UPDATE user SET password = "$hashed_password" WHERE superuser_access = 1;'
+mysql -u root -p@DB_PASSWORD@ matomo -e 'UPDATE user SET token_auth = "@MATOMO_ROOT_APIKEY@" WHERE superuser_access = 1;'
 rm /opt/matomo.sql
 rm /opt/install_matomo_db.sh
 rm /opt/*.html
