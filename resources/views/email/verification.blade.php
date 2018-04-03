8a1a4f3d80eeaf3a892a6dcdc8b1b25e636cbc90
Davide Porrovecchio <pdavide@gmail.com>
2018-04-03 17:12:01 +0200

    Fixed mail layout

--- resources/views/email/verification.blade.php
+++ resources/views/email/verification.blade.php
@@ -1,20 +1,20 @@
 @component('mail::message')
-    # Account creato
+# Account creato
 
-    Ciao {{ $user->name }} {{ $user->familyName }},
-    grazie per esserti registrato su {{ config('app.name') }}!
+Ciao {{ $user->name }} {{ $user->familyName }},
+grazie per esserti registrato su {{ config('app.name') }}!
 
-    Per completare la tua iscrizione è necessaria la
-    conferma del tuo indirizzo email.
+Per completare la tua iscrizione è necessaria la
+conferma del tuo indirizzo email.
 
-    @component('mail::button', ['url' => route('auth-do_verify', $token)])
-        Conferma indirizzo email
-    @endcomponent
+@component('mail::button', ['url' => route('auth-do_verify', $token)])
+    Conferma indirizzo email
+@endcomponent
 
-    Se non riesci a confermare cliccando sul bottone,
-    puoi visitare la pagina [{{ url('/verify') }}]({{ url('/verify') }})
-    ed inserire il seguente codice di verifica:
-    `{!! $token !!}`
+Se non riesci a confermare cliccando sul bottone,
+puoi visitare la pagina [{{ url('/verify') }}]({{ url('/verify') }})
+ed inserire il seguente codice di verifica:
+`{!! $token !!}`
 @endcomponent
 
 {{-- //TODO: put message in lang file --}}
