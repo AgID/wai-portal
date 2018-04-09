e38af772e5892b9f8fa09dffd511586d656cfaa6
Davide Porrovecchio <pdavide@gmail.com>
2018-04-10 00:26:01 +0200

    Fix tooltip width

--- resources/views/partials/user_info.blade.php
+++ resources/views/partials/user_info.blade.php
@@ -4,7 +4,7 @@
         <a href="#tooltip_status" class="Tooltip-toggle u-textClean u-padding-right-xs u-padding-left-xs u-color-black" data-menu-trigger="tooltip_status">
             {{ __('auth.status.'.auth()->user()->status) }}
         </a>
-        <span id="tooltip_status" data-menu class="Dropdown-menu u-borderShadow-m u-background-teal-70 u-color-white u-layout-prose u-padding-r-all u-borderRadius-l">
+        <span id="tooltip_status" data-menu class="Tooltip Dropdown-menu u-borderShadow-m u-background-teal-70 u-color-white u-layout-prose u-padding-r-all u-borderRadius-l">
             <span class="Icon-drop-down Dropdown-arrow u-color-teal-70"></span>
             <span class="u-layout-prose u-text-r-xs">
                 {{ __('auth.status.'.auth()->user()->status.'_description') }}
@@ -16,7 +16,7 @@
         <a href="#tooltip_role" class="Tooltip-toggle u-textClean u-padding-right-xs u-padding-left-xs u-color-black" data-menu-trigger="tooltip_role">
             {{ __('auth.roles.'.auth()->user()->roles()->first()->name) }}
         </a>
-        <span id="tooltip_role" data-menu class="Dropdown-menu u-borderShadow-m u-background-teal-70 u-color-white u-layout-prose u-padding-r-all u-borderRadius-l">
+        <span id="tooltip_role" data-menu class="Tooltip Dropdown-menu u-borderShadow-m u-background-teal-70 u-color-white u-layout-prose u-padding-r-all u-borderRadius-l">
             <span class="Icon-drop-down Dropdown-arrow u-color-teal-70"></span>
             <span class="u-layout-prose u-text-r-xs">
                 {{ __('auth.roles.'.auth()->user()->roles()->first()->name.'_description') }}
