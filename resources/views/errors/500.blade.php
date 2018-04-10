8ec268cd62076780e87d1a9e66ae796aa1abce56
Davide Porrovecchio <pdavide@gmail.com>
2018-04-10 23:13:10 +0200

    Add TODO

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="u-flex u-flexCol u-flexAlignItemsCenter u-margin-top-xxl">
         <p class="u-textWeight-600 u-textCenter u-text-xxl">{!! nl2br(__('ui.pages.500.description')) !!}</p>
-        <p class="u-color-90 u-text-xxl u-margin-top-xl">{{ $exception->getMessage() }}</p>
+        <p class="u-color-90 u-text-xxl u-margin-top-xl">{{ $exception->getMessage() }}</p> {{-- TODO: remove in production --}}
         <p><a href="{{ route('home', [], false) }}">{{ __('ui.return_home') }}</a></p>
     </div>
 @endsection
