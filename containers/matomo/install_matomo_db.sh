e9e2eae321475a416f69584353ca487128be1d13
Davide Porrovecchio <pdavide@gmail.com>
2019-01-30 14:08:12 +0100

    Cleanup matomo src folder

--- containers/matomo/install_matomo_db.sh
+++ containers/matomo/install_matomo_db.sh
@@ -5,4 +5,11 @@ mysql -u root -p@DB_PASSWORD@ matomo -e 'UPDATE user SET password = "@MATOMO_ROO
 mysql -u root -p@DB_PASSWORD@ matomo -e 'UPDATE user SET token_auth = "@MATOMO_ROOT_APIKEY@" WHERE superuser_access = 1;'
 rm /opt/matomo.sql
 rm /opt/install_matomo_db.sh
-rm /opt/*.html
+
+# Remove some files not related to web environment
+rm -f /opt/matomo/*.json
+rm -f /opt/matomo/*.lock
+rm -f /opt/matomo/*.md
+rm -f /opt/matomo/LEGALNOTICE
+rm -f /opt/matomo/LICENSE
+rm -Rf /opt/matomo/tests
