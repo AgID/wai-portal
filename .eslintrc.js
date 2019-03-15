3b00a80d2d3877c096f9e5946ba64f7c6e50dcea
Davide Porrovecchio <pdavide@gmail.com>
2019-03-15 12:40:15 +0100

    Update ESlint conf and change to js

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": [
+      "eslint:recommended"
+  ],
+  "env": {
+      "browser": true,
+      "jquery": true,
+      "commonjs": true,
+      "es6": true
+  },
+  "parser": "babel-eslint"
+};
