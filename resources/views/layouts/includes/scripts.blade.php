edc663da0a8cd4800c558b6cc85f420f9331f350
Davide Porrovecchio <pdavide@gmail.com>
2019-03-15 15:53:21 +0100

    Load vendor and manifest along with bundle

--- resources/views/layouts/includes/scripts.blade.php
+++ resources/views/layouts/includes/scripts.blade.php
@@ -1,3 +1,5 @@
 <script>__PUBLIC_PATH__ = "{{ asset('/toolkit') }}/"</script>
 <script type="text/javascript" src="{{ asset('/toolkit/IWT.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('/js/app.js') }}"></script>
+<script type="text/javascript" src="{{ mix('js/manifest.js') }}"></script>
+<script type="text/javascript" src="{{ mix('js/vendor.js') }}"></script>
+<script type="text/javascript" src="{{ mix('/js/app.js') }}"></script>
