32e141b1c627924291e81e44aff1e2ba07adfbb0
Davide Porrovecchio <pdavide@gmail.com>
2019-03-25 17:33:55 +0100

    Add new timestamps to user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\User;
+use Carbon\Carbon;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -11,18 +12,22 @@ $factory->define(User::class, function (Faker $faker) {
         'familyName' => $faker->lastName,
         'fiscalNumber' => $faker->taxId(),
         'email' => $faker->unique()->safeEmail,
+        'password_changed_at' => Carbon::now()->format('Y-m-d H:i:s'),
         'status' => 'inactive',
     ];
 });
 
 $factory->state(User::class, 'pending', [
+    'email_verified_at' => Carbon::now()->format('Y-m-d H:i:s'),
     'status' => 'pending',
 ]);
 
 $factory->state(User::class, 'active', [
+    'email_verified_at' => Carbon::now()->format('Y-m-d H:i:s'),
     'status' => 'active',
 ]);
 
 $factory->state(User::class, 'suspended', [
+    'email_verified_at' => Carbon::now()->format('Y-m-d H:i:s'),
     'status' => 'suspended',
 ]);
