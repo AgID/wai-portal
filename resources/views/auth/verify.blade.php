698117f0fc28477c3a74ef06a71d58529e7befb2
Davide Porrovecchio <pdavide@gmail.com>
2019-03-25 17:05:29 +0100

    Update verification view and delete unused ones

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -4,25 +4,11 @@
 
 @section('page-content')
 {{-- //TODO: allow to change email address --}}
-<form class="Form Form--spaced u-text-r-xs" method="get" action="{{ route('auth-do_verify', [], false) }}">
-    <fieldset class="Form-fieldset">
-        <div class="Form-field">
-            <label class="Form-label is-required" for="token">
-                Codice di verifica{{-- //TODO: put message in lang file --}}
-            </label>
-            <input class="Form-input" id="token" name="token" aria-required="true" required/>
-            <p class="Form-message">
-                Il codice di verifica ricevuto all'indirizzo {{ auth()->user()->email }}{{-- //TODO: put message in lang file --}}
-            </p>
-        </div>
-    </fieldset>
-    <div class="Form-field Grid-cell u-textCenter">
-        <button type="submit" class="Button Button--default u-text-xs submit">
-            Conferma{{-- //TODO: put message in lang file --}}
-        </button>
-        <a role="button" href="{{ route('auth-verify_resend', [], false) }}" class="Button Button--default u-text-xs submit">
-            Rispedisci codice di verifica{{-- //TODO: put message in lang file --}}
+    <div>
+        <p>Prima di continuare è necessario verificare la correttezza del tuo indirizzo email {{ $user->email }}.</p>
+        <p>Clicca sul link che abbiamo inviato alla tua casella di posta.{{-- //TODO: put message in lang file --}}</p>
+        <a role="button" href="{{ route($user->isA('super-admin') ? 'admin.verification.resend' : 'verification.resend', [], false) }}" class="Button Button--default u-text-xs submit">
+            Rispedisci mail di verifica{{-- //TODO: put message in lang file --}}
         </a>
     </div>
-</form>
 @endsection
