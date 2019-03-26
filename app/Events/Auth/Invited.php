2160b123c676ffc05fdbf8fe4ea3db8ebea7be69
Davide Porrovecchio <pdavide@gmail.com>
2019-03-26 19:00:55 +0100

    Update Invited event to support more informations

--- app/Events/Auth/Invited.php
+++ app/Events/Auth/Invited.php
@@ -2,30 +2,48 @@
 
 namespace App\Events\Auth;
 
-use Illuminate\Auth\Events\Registered;
+use App\Models\PublicAdministration;
+use App\Models\User;
 use Illuminate\Queue\SerializesModels;
 
-class Invited extends Registered
+class Invited
 {
     use SerializesModels;
 
     /**
+     * The invitated user.
+     *
+     * @var User
+     */
+    public $user;
+
+    /**
+     * The public administration selected for the invitation.
+     *
+     * @var App\Models\PublicAdministration
+     */
+    public $publicAdministration;
+
+    /**
      * The user issuing the invitation.
      *
-     * @var \Illuminate\Contracts\Auth\Authenticatable
+     * @var App\Models\User
      */
     public $invitedBy;
 
     /**
      * Create a new event instance.
      *
-     * @param \Illuminate\Contracts\Auth\Authenticatable $user
+     * @param App\Models\User $user
+     * @param App\Models\User $invitedBy
+     * @param App\Models\PublicAdministration $publicAdministration
      *
      * @return void
      */
-    public function __construct($user, $invitedBy)
+    public function __construct(User $user, PublicAdministration $publicAdministration = null, User $invitedBy)
     {
         $this->user = $user;
+        $this->publicAdministration = $publicAdministration;
         $this->invitedBy = $invitedBy;
     }
 }
