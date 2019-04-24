cb800e16ea7b67e857ad191cf7907c59eb553f2d
Davide Porrovecchio <pdavide@gmail.com>
2019-04-24 12:31:55 +0200

    Move interactions with IPA index in a new trait

--- app/Http/Controllers/SearchIPAListController.php
+++ app/Http/Controllers/SearchIPAListController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Ehann\RediSearch\Index;
-use Ehann\RedisRaw\PredisAdapter;
-use Exception;
+use App\Traits\InteractsWithIPAIndex;
 use Illuminate\Http\Request;
 
 class SearchIPAListController extends Controller
 {
+    use InteractsWithIPAIndex;
+
     /**
      * Return elements found in IPA list.
      *
@@ -18,28 +18,10 @@ class SearchIPAListController extends Controller
      */
     public function search(Request $request)
     {
-        $result = [];
         if (isset($request->q)) {
-            // Remove negation from query which can be slow and cause high CPU consumption
-            // See: http://redisearch.io/Query_Syntax/#pure"_"negative"_"queries
-            $query = str_replace('-', '', $request->q) . '*';
-
-            $IPAIndex = new Index((new PredisAdapter())->connect(config('database.redis.ipaindex.host'), config('database.redis.ipaindex.port'), config('database.redis.ipaindex.database')), 'IPAIndex');
-            try {
-                $result = $IPAIndex->limit(0, 100)
-                    ->sortBy('name')
-                    ->inFields(3, ['ipa_code', 'name', 'city'])
-                    ->search($query)
-                    ->getDocuments();
-            } catch (Exception $e) {
-                // RediSearch returned an error, probably malformed query or index not found.
-                // TODO: Please notify me!
-                if (!app()->environment('testing')) {
-                    logger()->error($e);
-                }
-            }
+            $result = $this->searchPublicAdministration($request->q);
         }
 
-        return response()->json($result);
+        return response()->json($result ?? []);
     }
 }
