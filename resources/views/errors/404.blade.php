1bfe0d6e188634528c1ed6d539cd2ffcd7275bb0
Davide Porrovecchio <pdavide@gmail.com>
2019-05-16 19:26:47 +0200

    Use default error handling for 404s

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="u-flex u-flexCol u-flexAlignItemsCenter">
         <p class="u-color-50 u-textWeight-600" style="font-size: 6em">404</p>
-        <p class="u-color-90 u-text-xxl u-margin-top-xl">{!! __('ui.pages.404.not_found', ['page' => '<code>'.$not_found_path.'</code>']) !!}</p>
+        <p class="u-color-90 u-text-xxl u-margin-top-xl">{!! __('ui.pages.404.not_found', ['page' => '<code>' . request()->path() . '</code>']) !!}</p>
         <p><a href="{{ route('home', [], false) }}">{{ __('ui.return_home') }}</a></p>
     </div>
 @endsection
