260d9879aa21ca1a61534c426aaef932b3f7a90d
Andrea Capelli <426288+Valair@users.noreply.github.com>
2019-10-23 16:21:36 +0200

    Disable internet access

--- containers/matomo/config.ini.php
+++ containers/matomo/config.ini.php
@@ -12,6 +12,7 @@ trusted_hosts[] = "localhost:9443"
 enable_trusted_host_check = 0
 force_ssl = 1
 enable_update_users_email = 0
+enable_internet_features = 0
 
 [Plugins]
 Plugins[] = "CorePluginsAdmin"
