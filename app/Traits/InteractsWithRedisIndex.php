c6d185d17c92d5d13c4d479ca5c4788186847dda
Andrea Capelli <426288+Valair@users.noreply.github.com>
2019-11-15 18:13:30 +0100

    Fix sentinels client selection

--- app/Traits/InteractsWithRedisIndex.php
+++ app/Traits/InteractsWithRedisIndex.php
@@ -297,7 +297,7 @@ trait InteractsWithRedisIndex
      */
     private function getConnectedRedisClient(string $index): RedisRawClientInterface
     {
-        if (('ipa' === $index && env('REDIS_REDISEARCH_USE_SENTINELS', false)) || (env('REDIS_INDEXES_USE_SENTINELS', false))) {
+        if (('ipa' === $index && env('REDIS_REDISEARCH_USE_SENTINELS', false)) || ('ipa' !== $index && env('REDIS_INDEXES_USE_SENTINELS', false))) {
             return (new RedisSentinelPredisAdapter($index))->connect();
         }
 
