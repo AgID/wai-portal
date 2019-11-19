7342e20078dabad426ebac6c2c5dfb400d3f39a2
Davide Porrovecchio <pdavide@gmail.com>
2019-11-19 14:55:33 +0100

    Fix typos

--- config/elastic-search.php
+++ config/elastic-search.php
@@ -3,7 +3,7 @@
 return [
     'host' => env('ELASTICSEARCH_HOST', '127.0.0.1'),
     'port' => env('ELASTICSEARCH_PORT', 9200),
-    'index_name' => env('ELASTICHSEARCH_INDEX_NAME', 'wai-log'),
-    'ignore_exceptions' => env('ELASTICSEARC_IGNORE_EXCEPTIONS', false),
+    'index_name' => env('ELASTICSEARCH_INDEX_NAME', 'wai-log'),
+    'ignore_exceptions' => env('ELASTICSEARCH_IGNORE_EXCEPTIONS', false),
     'search_template_name' => env('ELASTICSEARCH_SEARCH_TEMPLATE_NAME', 'log_search'),
 ];
