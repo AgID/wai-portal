1d8a1a884ad31e8fbebf344f46623ebe6a7f5ade
Andrea Capelli <426288+Valair@users.noreply.github.com>
2019-11-26 17:07:21 +0100

    Remove unused helper

--- app/helpers.php
+++ app/helpers.php
@@ -18,24 +18,3 @@ if (!function_exists('current_public_administration')) {
         return null;
     }
 }
-
-if (!function_exists('current_user_auth_token')) {
-    /**
-     * Get the Analytics Service authentication token for the current user.
-     *
-     * @throws \Illuminate\Contracts\Container\BindingResolutionException if unable to bind to the service
-     * @throws \App\Exceptions\AnalyticsServiceException if unable to contact the Analytics Service
-     * @throws \App\Exceptions\CommandErrorException if command finishes with error
-     *
-     * @return string|null the user authentication token or null if unable to retrieve
-     */
-    function current_user_auth_token(): ?string
-    {
-        $user = auth()->user();
-        if ($user) {
-            return $user->getAnalyticsServiceAccountTokenAuth();
-        }
-
-        return null;
-    }
-}
