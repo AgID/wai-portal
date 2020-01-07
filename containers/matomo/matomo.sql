137168a6789d17b503a35ec55f7093d9fd8825ac
Andrea Capelli <426288+Valair@users.noreply.github.com>
2020-01-07 14:46:25 +0100

    Add default anonymous user redirect to login

--- containers/matomo/matomo.sql
+++ containers/matomo/matomo.sql
@@ -498,7 +498,8 @@ INSERT INTO `option` VALUES
   ('version_VisitsSummary','3.13.0',1),
   ('version_VisitTime','3.13.0',1),
   ('version_WebsiteMeasurable','3.13.0',1),
-  ('version_Widgetize','3.13.0',1);
+  ('version_Widgetize','3.13.0',1),
+  ('anonymous_defaultReport','Login',0);
 /*!40000 ALTER TABLE `option` ENABLE KEYS */;
 UNLOCK TABLES;
 
