21ded4e946e9c2dc8053cc57287b0c0daa547793
Andrea Capelli <426288+Valair@users.noreply.github.com>
2020-01-28 14:27:23 +0100

    Add testing support method

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,16 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Log;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function expectLogMessage(string $logLevel, $args, int $times = 1): void
+    {
+        Log::shouldReceive($logLevel)
+            ->times($times)
+            ->withArgs($args);
+    }
 }
