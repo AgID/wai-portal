2b2bc7f4bab86761cbb948990815426372c94c9e
Andrea Capelli <426288+Valair@users.noreply.github.com>
2020-02-06 10:42:24 +0100

    Fix wrong locale

--- resources/views/mail/partials/en/public_administration/registered_message.blade.php
+++ resources/views/mail/partials/en/public_administration/registered_message.blade.php
@@ -3,7 +3,7 @@ Hello {{ $user->name }},
 the public administration **{{ $publicAdministration->name }}** has been
 correctly registered to [{{ config('app.name') }}]({{ url('/') }}).
 
-@include('mail.partials.it.website.tracking_instructions', ['javascriptSnippet' => $javascriptSnippet])
+@include('mail.partials.en.website.tracking_instructions', ['javascriptSnippet' => $javascriptSnippet])
 
 For more support on this topic, you can browse
 the [{{ config('app.name_short') }} guide]({{ config('site.kb.link') }}).
