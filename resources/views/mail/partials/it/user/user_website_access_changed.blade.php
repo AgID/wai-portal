f9095c3e93d1c6d3cd77a10dd12493d809bd4d6d
Andrea Capelli <426288+Valair@users.noreply.github.com>
2020-02-25 12:12:43 +0100

    Fix mail template parameter

--- resources/views/mail/partials/it/user/user_website_access_changed.blade.php
+++ resources/views/mail/partials/it/user/user_website_access_changed.blade.php
@@ -4,4 +4,4 @@ ti informiamo che i permessi dell'utente **{{ $modifiedUser->full_name }}** sono
 stati modificati.
 
 Puoi verificare i nuovi permessi dal suo
-[profilo]({{ url(route('users.show', ['user' => $this->invitedUser])) }}).
+[profilo]({{ url(route('users.show', ['user' => $modifiedUser])) }}).
