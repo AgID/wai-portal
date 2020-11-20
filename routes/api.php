9fd2087e2b669a60f359516f48113329f96d0fb9
Giampaolo Fois <giampaolo.fois@softfobia.com>
2020-11-20 17:01:49 +0100

    Define API routes and Controllers

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -10,3 +12,55 @@
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::middleware('client')->group(function () {
+    Route::middleware('api.authentication')->group(function () {
+        Route::prefix('/users')->group(function () {
+            Route::get('/', 'UserController@dataApiJson')
+                ->name('api.users');
+            Route::post('/', 'UserController@storeJson')
+                ->name('api.users.store');
+            Route::get('/{fn}', 'UserController@showJson')
+                ->name('api.users.show');
+            Route::patch('/{fn}', 'UserController@updateApi')
+                ->name('api.users.update');
+            Route::delete('/{fn}', 'UserController@deleteApi')
+                ->name('api.users.delete');
+        });
+        Route::prefix('/sites')->group(function () {
+            Route::post('/', 'WebsiteController@storeApi')
+                ->name('api.sites.update');
+            Route::get('/', 'WebsiteController@dataApi')
+                ->name('api.sites.show');
+            Route::get('/{website}', 'WebsiteController@showApi')
+                ->name('api.sites.read');
+            Route::put('/{website}', 'WebsiteController@updateApi')
+                ->name('api.sites.update');
+            Route::patch('/{website}/archive', 'WebsiteController@archive')
+                ->name('api.sites.archive');
+            Route::patch('/{website}/unarchive', 'WebsiteController@unarchive')
+                ->name('api.sites.unarchive');
+            Route::get('/{website}/check', 'WebsiteController@checkTracking')
+                ->name('api.sites.check');
+            Route::get('/{website}/force', 'WebsiteController@forceActivation')
+                ->name('api.sites.force');
+        });
+
+        /* Route::prefix('/test')->group(function () {
+            Route::get('/', function () {
+                return response()->json(['test' => 'Hello World'], 200);
+            });
+            Route::get('/update', 'UserController@update')
+                ->name('api.users.test.update');
+            //show test
+            Route::get('/show/{fn}', 'UserController@showJson')
+                ->name('api.users.test.show');
+
+            Route::get('/data', 'UserController@apiData')
+                ->name('api.users.test.apiData');
+            Route::get('/test', function () {
+                return response()->json(['test' => 'Hello World'], 200);
+            });
+        }); */
+    });
+});
