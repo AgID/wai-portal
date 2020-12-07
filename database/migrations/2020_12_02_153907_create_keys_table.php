a9971797573210385cb843e988a8ea84730d3c3c
Giampaolo Fois <giampaolo.fois@softfobia.com>
2020-12-07 18:53:20 +0100

    Add migration for keys table

---
+++ database/migrations/2020_12_02_153907_create_keys_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKeysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('keys', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('client_name');
+            $table->string('consumer_id');
+            $table->integer('public_administration_id')->unsigned()->index();
+            $table->timestamps();
+            $table->foreign('public_administration_id')->references('id')->on('public_administrations')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('keys');
+    }
+}
