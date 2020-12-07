49221157df8e5ae7c4f07bc5acd8ab5b1f775a2b
Giampaolo Fois <giampaolo.fois@softfobia.com>
2020-12-07 18:58:17 +0100

    Add API swagger

--- resources/views/pages/swagger.blade.php
+++ resources/views/pages/swagger.blade.php
@@ -6,6 +6,14 @@
 <div class="row">
     <div class="col-lg-12 d-flex">
         @component('layouts.components.box')
+        <div class="mt-4 text-center text-sm-right">
+            @component('layouts.components.link_button', [
+                'link' => $keys,
+                'size' => 'lg',
+            ])
+            {{ ucfirst(__('Gestione delle credenziali OAuth')) }}
+            @endcomponent
+        </div>
         <div id="swagger-ui" />
         @endcomponent
     </div>
