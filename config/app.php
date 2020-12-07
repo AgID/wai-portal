5470c4b31cb4ccd360cbf28346b07d4c9f2242f4
Giampaolo Fois <giampaolo.fois@softfobia.com>
2020-12-07 18:59:30 +0100

    Add kong service

--- config/app.php
+++ config/app.php
@@ -195,6 +195,7 @@ return [
         /*
          * Application Service Providers...
          */
+        App\Providers\KongClientProvider::class,
         App\Providers\AnalyticsServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
---
+++ config/kong-service.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'endpoint_url' => env('KONG_ENDPOINT_URL')
+];
