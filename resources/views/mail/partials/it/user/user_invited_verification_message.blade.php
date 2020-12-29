295cbe5c6b08b2cc4ecdc16b47852459c3863a75
Davide Porrovecchio <pdavide@gmail.com>
2020-12-29 17:33:25 +0100

    Add mail template for user invitations

---
+++ resources/views/mail/partials/it/user/user_invited_verification_message.blade.php
@@ -0,0 +1,20 @@
+Ciao {{ $user->full_name }},
+
+sei stato invitato su [{{ config('app.name') }}]({{ url('/') }}).
+
+Per completare la procedura conferma il tuo indirizzo email.
+
+@component('mail::button', ['link' => $signedUrl])
+    Conferma email
+@endcomponent
+
+Se non riesci a confermare cliccando sul bottone,
+copia e incolla questo link nella barra degli indirizzi del
+browser: `{!! $signedUrl !!}`
+
+**Puoi confermare l'indirizzo email
+entro {{ config('auth.verification.expire', 7) }} giorni dalla ricezione di questo
+messaggio.**
+
+*Se pensi di aver ricevuto questo messaggio per errore puoi scrivere a
+{{ config('site.owner.mail') }}.*
