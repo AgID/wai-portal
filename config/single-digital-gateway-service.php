03abd75308f51fe07dd73a939b6ac223c17e8f1a
Davide Porrovecchio <pdavide@gmail.com>
2021-01-11 21:35:51 +0100

    Make SDG csv column separator configurable

--- config/single-digital-gateway-service.php
+++ config/single-digital-gateway-service.php
@@ -7,7 +7,8 @@ return [
     'last_days' => env('SDG_API_LAST_DAYS'),
     'start_date' => env('SDG_API_START_DATE'),
     'end_date' => env('SDG_API_END_DATE'),
-    'storage_folder' => env('SDG_STORAGE_FOLDER'),
-    'storage_disk' => env('SDG_STORAGE_DISK'),
-    'url_column_index_csv' => env('SDG_COLUMN_INDEX_URL_CSV')
+    'storage_folder' => env('SDG_STORAGE_FOLDER', 'sdg'),
+    'storage_disk' => env('SDG_STORAGE_DISK', 'persistent'),
+    'url_column_index_csv' => env('SDG_COLUMN_INDEX_URL_CSV'),
+    'url_column_separator_csv' => env('SDG_COLUMN_SEPARATOR_CSV', ','),
 ];
