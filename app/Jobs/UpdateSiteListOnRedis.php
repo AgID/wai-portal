ddf67cac6a509b52937c7a489e1bef8b32f83bbd
Giampaolo Fois <giampaolo.fois@softfobia.com>
2021-01-11 13:00:30 +0100

    Add fqdn list to redis cache

--- app/Jobs/UpdateSiteListOnRedis.php
+++ app/Jobs/UpdateSiteListOnRedis.php
@@ -2,14 +2,13 @@
 
 namespace App\Jobs;
 
-use App\Models\PublicAdministration;
+use App\Models\Website;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Redis;
-use Illuminate\Support\Str;
 
 class UpdateSiteListOnRedis implements ShouldQueue
 {
@@ -18,42 +17,36 @@ class UpdateSiteListOnRedis implements ShouldQueue
     use Queueable;
     use SerializesModels;
 
+    protected $analyticsService;
+
+    public function __construct()
+    {
+        $this->analyticsService = app()->make('analytics-service');
+    }
+
     /**
      * Execute the job.
      */
     public function handle(): void
     {
-        $publicAdministrationsList = PublicAdministration::all();
-
-        Redis::connection(env('CACHE_CONNECTION'))->client()->pipeline(function ($pipe) use ($publicAdministrationsList) {
-            foreach ($publicAdministrationsList as &$publicAdministration) {
-                $list = $publicAdministration->websites()->get()
-                    ->map(function ($website) {
-                        $url = collect($website->toArray())
-                            ->only(['url'])
-                            ->all();
-
-                        $url = $url['url'];
-
-                        $hasProtocol = Str::startsWith($url, 'http');
-
-                        return $hasProtocol ? $url : 'http://' . $url . ' ' . 'https://' . $url;
-                    })->values()->toArray();
-
-                $id = $publicAdministration['id'];
+        $websiteList = Website::all();
+        /*
+                per ogni website predere l'id e fare una chiamata a matomo per prendere la lista dei domini
+                145 sito1 sito2
+            */
+        Redis::connection(env('CACHE_CONNECTION'))->client()->pipeline(function ($pipe) use ($websiteList) {
+            foreach ($websiteList as &$website) {
+                $id = $website->analytics_id;
+                $list = $this->analyticsService->getSiteUrlsFromId($id);
 
                 $listToString = implode(' ', $list);
 
-                logger()->notice(
-                    'Caching websites for public administrations',
-                    [
-                        'pa' => $id,
-                        'list' => $listToString,
-                    ]
-                );
-
-                $pipe->set('websiteList-' . $id, $listToString);
+                $pipe->set($id, $listToString);
             }
         });
+
+        logger()->notice(
+            'Caching websites for public administrations'
+        );
     }
 }
