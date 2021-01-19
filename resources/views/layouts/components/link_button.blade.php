501ffdc62ba55bb2322e26a99d317221566045ce
Giampaolo Fois <giampaolo.fois@softfobia.com>
2021-01-19 17:20:33 +0100

    Navbar API link only visible to signed users

--- resources/views/layouts/components/link_button.blade.php
+++ resources/views/layouts/components/link_button.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ $link }}" role="button" class="btn btn-{{ $type ?? 'primary' }}{{ empty($icon) ? '' : ' btn-icon' }}{{ isset($size) ? " btn-{$size}" : ''}}">
+<a href="{{ $link }}" role="button" class="btn btn-{{ $type ?? 'primary' }}{{ empty($icon) ? '' : ' btn-icon' }}{{ isset($size) ? " btn-{$size}" : ''}} {{ isset($disabled) && $disabled ? " disabled" : ''}}">
     <span>{{ ucfirst($slot) }}</span>
     @isset($icon)
     <svg class="icon icon-{{ $iconColor ?? 'white' }} ml-1">
