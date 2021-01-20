15a5d92c206fdc9c878472849223237258cc2911
Giampaolo Fois <giampaolo.fois@softfobia.com>
2021-01-20 17:40:24 +0100

    Add kong to swagger server list

--- resources/js/swagger.js
+++ resources/js/swagger.js
@@ -6,11 +6,19 @@ export default (() => {
     const init = () => {
         const swaggerDiv = document.getElementById('swagger-ui');
         
+        const apiUrl = swaggerDiv.hasAttribute("data-url") ? swaggerDiv.getAttribute("data-url") : "localhost";
+
+        spec.servers.push(
+            {
+                "url": apiUrl + "/portal",
+                "description": "Kong Portal Gateway"
+            }
+        );
+
         if(swaggerDiv){
             SwaggerUIBundle({
-                //url: "https://petstore.swagger.io/v2/swagger.json",
                 spec,
-                dom_id: '#swagger-ui'
+                dom_id: '#swagger-ui',
               })
         }
     };
