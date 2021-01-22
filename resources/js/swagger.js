7642f748b32c66d43089911d1420a472123225e6
Giampaolo Fois <giampaolo.fois@softfobia.com>
2021-01-22 12:22:16 +0100

    Fix javascript null error

--- resources/js/swagger.js
+++ resources/js/swagger.js
@@ -1,25 +1,26 @@
-import SwaggerUIBundle from 'swagger-ui';
-import 'swagger-ui/dist/swagger-ui.css';
-import spec from '../data/api.json'
+import SwaggerUIBundle from "swagger-ui";
+import "swagger-ui/dist/swagger-ui.css";
+import spec from "../data/api.json";
 
 export default (() => {
     const init = () => {
-        const swaggerDiv = document.getElementById('swagger-ui');
-        
-        const apiUrl = swaggerDiv.hasAttribute("data-url") ? swaggerDiv.getAttribute("data-url") : "localhost";
+        const swaggerDiv = document.getElementById("swagger-ui");
 
-        spec.servers.push(
-            {
-                "url": apiUrl + "/portal",
-                "description": "Kong Portal Gateway"
-            }
-        );
+        const apiUrl =
+            swaggerDiv && swaggerDiv.hasAttribute("data-url")
+                ? swaggerDiv.getAttribute("data-url")
+                : "localhost";
 
-        if(swaggerDiv){
+        spec.servers.push({
+            url: apiUrl + "/portal",
+            description: "Kong Portal Gateway"
+        });
+
+        if (swaggerDiv) {
             SwaggerUIBundle({
                 spec,
-                dom_id: '#swagger-ui',
-              })
+                dom_id: "#swagger-ui"
+            });
         }
     };
 
