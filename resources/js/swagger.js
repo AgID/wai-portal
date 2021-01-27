671c2af5b94f4a0eb47e300d9a027a757ed43799
Giampaolo Fois <giampaolo.fois@softfobia.com>
2021-01-27 16:52:49 +0100

    Add key selection to swagger

--- resources/js/swagger.js
+++ resources/js/swagger.js
@@ -1,26 +1,94 @@
+import axios from "axios";
 import SwaggerUIBundle from "swagger-ui";
 import "swagger-ui/dist/swagger-ui.css";
 import spec from "../data/api.json";
+
+const getKeyInfo = async consumerId => {
+    let response = {};
+
+    try {
+        response = await axios({
+            method: "GET",
+            url: `/api-key/${consumerId}/show/json`,
+            headers: {
+                accept: "*/*",
+                "content-type": "application/json"
+            },
+            timeout: 3000
+        });
+    } catch (error) {
+        response.data = {
+            error: true,
+            ...(error?.response?.data && { message: error.response.data })
+        };
+    }
+
+    const data = await response.data;
+
+    return data;
+};
 
 export default (() => {
     const init = () => {
         const swaggerDiv = document.getElementById("swagger-ui");
 
-        const apiUrl =
-            swaggerDiv && swaggerDiv.hasAttribute("data-url")
+        if (swaggerDiv) {
+            const apiUrl = swaggerDiv.hasAttribute("data-url")
                 ? swaggerDiv.getAttribute("data-url")
                 : "localhost";
 
-        spec.servers.push({
-            url: apiUrl + "/portal",
-            description: "Kong Portal Gateway"
-        });
+            const production = swaggerDiv.hasAttribute("data-environment")
+                ? swaggerDiv.getAttribute("data-environment")
+                : "false";
 
-        if (swaggerDiv) {
-            SwaggerUIBundle({
-                spec,
-                dom_id: "#swagger-ui"
+            const isProduction = production === "true" ? true : false;
+
+            const selectKey = document.getElementById("select-key");
+            const getKey = document.getElementById("use-key");
+
+            spec.servers.push({
+                url: apiUrl + "/portal",
+                description: "Kong Portal Gateway"
             });
+
+            spec.components.securitySchemes.oAuthSample.flows.clientCredentials.tokenUrl =
+                apiUrl + "/portal/oauth2/token";
+
+            const disableTryItOutPlugin = () => {
+                return {
+                    statePlugins: {
+                        spec: {
+                            wrapSelectors: {
+                                allowTryItOutFor: () => () => !isProduction
+                            }
+                        }
+                    }
+                };
+            };
+
+            const ui = SwaggerUIBundle({
+                spec,
+                dom_id: "#swagger-ui",
+                plugins: [disableTryItOutPlugin]
+            });
+
+            if (selectKey && !isProduction) {
+                getKey.addEventListener("click", async () => {
+                    const { key } = await getKeyInfo(selectKey.value);
+
+                    ui.initOAuth({
+                        clientId: key?.client_id,
+                        clientSecret: key?.client_secret,
+                        appName: key?.name,
+                        scopeSeparator: " ",
+                        scopes: "user",
+                        additionalQueryStringParams: {
+                            grant_type: "client_credentials"
+                        },
+                        usePkceWithAuthorizationCodeGrant: false
+                    });
+                });
+            }
         }
     };
 
