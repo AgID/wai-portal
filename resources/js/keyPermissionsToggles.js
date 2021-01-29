f6069761f7863ce577ba4bd03c1eb19ee0428e99
Giampaolo Fois <giampaolo.fois@softfobia.com>
2021-01-29 11:03:39 +0100

    Admin key type have all permissions

--- resources/js/keyPermissionsToggles.js
+++ resources/js/keyPermissionsToggles.js
@@ -1,21 +1,49 @@
 export default (() => {
     const initTogglesKeys = () => {
-        const adminSelect = document.getElementById('type');
-        const permissionsToggles = [...document.querySelectorAll(`input[type="checkbox"][name^="permissions"]:not([disabled])`)];
+        const adminSelect = document.getElementById("type");
+        const permissionsToggles = [
+            ...document.querySelectorAll(
+                `input[type="checkbox"][name^="permissions"]:not([disabled])`
+            )
+        ];
 
         permissionsToggles.map(toggle => {
-            toggle.addEventListener('change', event => {
+            toggle.addEventListener("change", event => {
+                if (adminSelect.value === "admin") {
+                    event.target.checked = true;
+                    return;
+                }
                 if (!event.currentTarget.checked) {
                     adminSelect && (adminSelect.value = "analytics");
                 }
-                if ('R' === event.currentTarget.value && !event.currentTarget.checked) {
-                    document.getElementById(`permissions[${event.currentTarget.dataset.entity}][]-W`).checked = false;
+
+                if (
+                    "R" === event.currentTarget.value &&
+                    !event.currentTarget.checked
+                ) {
+                    document.getElementById(
+                        `permissions[${event.currentTarget.dataset.entity}][]-W`
+                    ).checked = false;
                 }
-                if ('W' === event.currentTarget.value && event.currentTarget.checked) {
-                    document.getElementById(`permissions[${event.currentTarget.dataset.entity}][]-R`).checked = true;
+                if (
+                    "W" === event.currentTarget.value &&
+                    event.currentTarget.checked
+                ) {
+                    document.getElementById(
+                        `permissions[${event.currentTarget.dataset.entity}][]-R`
+                    ).checked = true;
                 }
-            })
+            });
         });
+
+        adminSelect &&
+            adminSelect.addEventListener("change", event => {
+                permissionsToggles.map(toggle => {
+                    toggle.checked = event.target.value === "admin";
+                    toggle.readOnly =
+                        event.target.value === "admin" ? "readonly" : "";
+                });
+            });
 
         permissionsToggles && (permissionsToggles.disabled = false);
     };
