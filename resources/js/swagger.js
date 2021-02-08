0d4a35814e638cc4c4e7216c2300184b11b0a19d
Davide Porrovecchio <pdavide@gmail.com>
2021-02-08 19:32:44 +0100

    Fix API base url

--- resources/js/swagger.js
+++ resources/js/swagger.js
@@ -43,7 +43,7 @@ export default (() => {
             );
 
             spec.servers.push({
-                url: apiUrl + "/portal",
+                url: apiUrl,
                 description: "API Gateway"
             });
 
