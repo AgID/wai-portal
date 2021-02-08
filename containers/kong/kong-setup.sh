d6396ff710cf4fa87012714555c4357809ba7789
Davide Porrovecchio <pdavide@gmail.com>
2021-02-08 19:43:12 +0100

    Remove reduntant option

--- containers/kong/kong-setup.sh
+++ containers/kong/kong-setup.sh
@@ -8,10 +8,10 @@ until $(curl --output /dev/null --silent --head --fail http://localhost:8001/ser
 done
 
 if ! curl -s http://localhost:8001/services | grep -q "nginx"; then
-    curl --output /dev/null --silent -X POST -s http://localhost:8001/plugins/ --data "name=cors&config.origins=https://localhost"
-    curl --output /dev/null --silent -X POST -s http://localhost:8001/plugins/ --data "name=oauth2&config.enable_client_credentials=true&config.global_credentials=true&config.accept_http_if_already_terminated=true"
-    curl --output /dev/null --silent -X POST -s http://localhost:8001/services --data "name=portal&url=https://nginx/api"
-    curl --output /dev/null --silent -X POST -s http://localhost:8001/services/portal/routes --data "paths[]=/portal&name=portal"
+    curl --output /dev/null --silent -X POST http://localhost:8001/plugins/ --data "name=cors&config.origins=https://localhost"
+    curl --output /dev/null --silent -X POST http://localhost:8001/plugins/ --data "name=oauth2&config.enable_client_credentials=true&config.global_credentials=true&config.accept_http_if_already_terminated=true"
+    curl --output /dev/null --silent -X POST http://localhost:8001/services --data "name=portal&url=https://nginx/api"
+    curl --output /dev/null --silent -X POST http://localhost:8001/services/portal/routes --data "paths[]=/portal&name=portal"
 fi
 
 echo "Kong plugins and routes are ready"
