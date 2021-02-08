2658778c7a1b964a3002d3ac96e11d4cfdb7130c
Davide Porrovecchio <pdavide@gmail.com>
2021-02-08 13:56:38 +0100

    Fix letter case and whitespaces

--- resources/views/pages/credentials/partials/form.blade.php
+++ resources/views/pages/credentials/partials/form.blade.php
@@ -4,24 +4,24 @@
     @method('put')
     @endisset
     @component('layouts.components.box', ['classes' => 'rounded'])
-    <h3 class="section-header">{{__('Informazioni Credenziale')}}</h3>
+    <h3 class="section-header">{{__('Informazioni credenziale')}}</h3>
     <div class="form-row">
         <div class="form-group has-form-text col-md-7">
             <div class="input-group">
                 <div class="input-group-prepend">
                     <div class="input-group-text"><svg class="icon icon-sm"><use xlink:href="{{ asset('svg/sprite.svg#it-help-circle') }}"></use></svg></div>
                 </div>
-                <label for="credential_name">{{ ucfirst(__('nome della Credenziale')) }}</label>
-                <input 
-                    type="text" 
-                    class="form-control{{ $errors->has('credential_name') ? ' is-invalid' : '' }}" 
-                    id="credential_name" 
-                    name="credential_name" 
-                    value="{{ old('credential_name', $credential->client_name ?? '') }}" 
-                    maxlength="255" 
-                    aria-describedby="name-input-help" 
-                    aria-required="true" 
-                    required 
+                <label for="credential_name">{{ ucfirst(__('nome della credenziale')) }}</label>
+                <input
+                    type="text"
+                    class="form-control{{ $errors->has('credential_name') ? ' is-invalid' : '' }}"
+                    id="credential_name"
+                    name="credential_name"
+                    value="{{ old('credential_name', $credential->client_name ?? '') }}"
+                    maxlength="255"
+                    aria-describedby="name-input-help"
+                    aria-required="true"
+                    required
                     >
                 @error('credential_name')
                 <div class="invalid-feedback">{{ $errors->first('credential_name') }}</div>
@@ -29,12 +29,12 @@
                 <div class="invalid-feedback">{{ __('validation.required', ['attribute' => __('validation.attributes.credential_name')]) }}</div>
                 @enderror
             </div>
-            <small id="name-input-help" class="form-text text-muted">{{ __('Inserisci il nome della Credenziale.') }}</small>
+            <small id="name-input-help" class="form-text text-muted">{{ __('Scegli un nome per la credenziale.') }}</small>
         </div>
     </div>
     <div class="form-row">
         <h4 class="text-uppercase m-0">{{ __('PERMESSI SUI SITI WEB') }}</h4>
-        
+
     </div>
     <div class="form-row mt-5">
         <div class="form-group has-form-text col-md-7">
@@ -45,18 +45,18 @@
                 <div class="bootstrap-select-wrapper form-control flex-grow-1{{ $errors->has('type') ? ' is-invalid' : '' }}">
                     <label for="type">{{ ucfirst(__('tipologia')) }}</label>
                     <select title="Scegli una tipologia" id="type" name="type" aria-required="true" required>
-                        <option 
+                        <option
                         value="admin"
                         @isset($type)
                             {{ $type === "admin" ? "selected" : "" }}
                         @endisset
-                        
+
                         >
                             Amministratore
                         </option>
-                        <option 
+                        <option
                         value="analytics"
-                        @isset($type) 
+                        @isset($type)
                             {{ $type === "analytics" ? "selected" : "" }}
                         @endisset
                         >
@@ -72,7 +72,7 @@
             </div>
             <small id="type-input-help" class="form-text text-muted">
                 {{ __('L\'amministratore può gestire tutti i siti web e tutti gli utenti') }}
-            </small>     
+            </small>
         </div>
     </div>
     <div class="form-row">
@@ -84,6 +84,6 @@
         </div>
     </div>
     @endcomponent
-    
+
 </form>
 
