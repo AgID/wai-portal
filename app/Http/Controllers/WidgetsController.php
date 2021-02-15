f71e891397a511c38b4f7fe11bdde0f44408b2c8
Giampaolo Fois <giampaolo.fois@softfobia.com>
2021-02-15 15:32:11 +0100

    Handle widgets preview error

--- app/Http/Controllers/WidgetsController.php
+++ app/Http/Controllers/WidgetsController.php
@@ -2,18 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\UserPermission;
+use App\Enums\WebsiteStatus;
+use App\Models\PublicAdministration;
 use App\Models\Website;
 use Symfony\Component\Yaml\Yaml;
+use App\Traits\HasRoleAwareUrls;
 
 class WidgetsController extends Controller
 {
+    use HasRoleAwareUrls;
+
     public function __construct()
     {
         $this->analyticsService = app()->make('analytics-service');
     }
 
-    public function index(Website $website)
+    public function index(Website $website, PublicAdministration $publicAdministration)
     {
+        $publicAdministration = auth()->user()->can(UserPermission::ACCESS_ADMIN_AREA)
+            ? $publicAdministration
+            : current_public_administration();
+
+        if($website->status->is(WebsiteStatus::PENDING)){
+            $redirectUrl = $this->getRoleAwareUrl('websites.show', ['website' => $website], $publicAdministration);
+            $name = $website->name;
+            
+            return redirect()->to($redirectUrl)->withNotification([
+                'title' => __('Errore'),
+                'message' => __("Attiva il sito '$name' prima di accedere all'anteprima dei widget."),
+                'status' => 'error',
+                'icon' => 'it-close-circle',
+            ]);
+        }
+        
         $analyticsId = $website->analytics_id;
         $widgetData = $this->analyticsService->getWidgetMetadata($analyticsId);
         $matomoWidgetUrl = config('analytics-service.widgets_url');
