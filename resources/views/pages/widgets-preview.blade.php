359b296129ce6e20ea98a4e0eb6e781e19402781
Davide Porrovecchio <pdavide@gmail.com>
2021-02-15 12:14:02 +0100

    Update widgets text content

--- resources/views/pages/widgets-preview.blade.php
+++ resources/views/pages/widgets-preview.blade.php
@@ -1,40 +1,39 @@
 @extends('layouts.page', ['graphicBackground' => true])
 
-@section('title', 'Preview Widgets')
+@section('title', 'Anteprima widget')
 
 @section('content')
     <div class="row" id="widgets-preview-box" data-url="{{$url}}">
         <div class="col-lg-12 d-flex">
             @component('layouts.components.box')
                 <div class="card-wrapper card-space">
-                    <div class="card card-bg">
+                    <div class="card card-bg no-after">
                         <div class="card-body">
-                            <h5 class="card-title">Preview Widget</h5>
+                            <h5 class="card-title">{{ __('Anteprima') }}</h5>
                             <p id="widget-subtitle-1"></p>
                             <div id="widget-preview" class="mt-4">
-                                Nessun widget selezionato
+                                {{ __('Nessun widget selezionato') }}
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="card-wrapper card-space">
-                    <div class="card card-bg">
+                <div class="card-wrapper card-space mb-5">
+                    <div class="card card-bg no-after">
                         <div class="card-body">
-                            <h5 class="card-title">Codice HTML Widget</h5>
+                            <h5 class="card-title">{{ __('Codice HTML del widget') }}</h5>
                             <p id="widget-subtitle-2"></p>
                             <code id="widget-code" class="mt-4">
-                                Nessun widget selezionato 
+                                {{ __('Nessun widget selezionato') }}
                             </code>
                         </div>
                     </div>
                 </div>
-                <h3>Widget disponibili</h3>
-                <p>Seleziona un widget per vederne la preview e ottenere il codice iframe da applicare nel sito
-                    dell'amministrazione</p>
+                <h3>{{ __('Widget disponibili') }}</h3>
+                <p>{{ __("Seleziona un widget per vederne l'anteprima e ottenere il codice HTML da inserire nel sito web.") }}</p>
                 <div class="row">
                     @foreach ($widgets as $key => &$widget)
-                    @if (is_array($allowedWidgets) && in_array($widget["uniqueId"], $allowedWidgets)) 
-                        <div class="col-4">
+                    @if (is_array($allowedWidgets) && in_array($widget["uniqueId"], $allowedWidgets))
+                        <div class="col-md-4">
                             <div class="btn btn-block border rounded mb-4 text-center pointer" id="widget-{{ $key }}"
                                 data-type="widget-select" data-attribute="back-to-top">
                                 <span style="display: none;" id="widget-json-{{ $key }}"
