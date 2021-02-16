d92ac4f0bdf8a8f9249d13c4f029df640c6a12d5
Davide Porrovecchio <pdavide@gmail.com>
2021-02-16 11:44:42 +0100

    Add matomo api service from wai-api

--- containers/kong/kong-setup.sh
+++ containers/kong/kong-setup.sh
@@ -12,6 +12,8 @@ if ! curl -s http://localhost:8001/services | grep -q "nginx"; then
     curl --output /dev/null --silent -X POST http://localhost:8001/plugins/ --data "name=oauth2&config.enable_client_credentials=true&config.global_credentials=true&config.accept_http_if_already_terminated=true"
     curl --output /dev/null --silent -X POST http://localhost:8001/services --data "name=portal&url=https://nginx/api"
     curl --output /dev/null --silent -X POST http://localhost:8001/services/portal/routes --data "paths[]=/portal&name=portal"
+    curl --output /dev/null --silent -X POST http://localhost:8001/services --data "name=matomo&url=http://express:7080"
+    curl --output /dev/null --silent -X POST http://localhost:8001/services/matomo/routes --data "paths[]=/matomo&name=matomo"
 fi
 
 echo "Kong plugins and routes are ready"
