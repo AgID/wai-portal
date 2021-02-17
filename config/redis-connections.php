3635c37d3fbb2ac8616a2f410774eb923fe7aba2
Giampaolo Fois <giampaolo.fois@softfobia.com>
2021-02-17 16:06:15 +0100

    User laravel cache facade

--- config/redis-connections.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'cache_connection' => env('CACHE_CONNECTION'),
-    'session_connection' => env('SESSION_CONNECTION'),
-    'queue_redis_connection' => env('QUEUE_REDIS_CONNECTION')
-];
