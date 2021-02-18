0c019ab8774b31efe841b7af8065a3dd8e7dd6ec
Davide Porrovecchio <pdavide@gmail.com>
2021-02-18 13:21:52 +0100

    Rename middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -80,7 +80,7 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'cron.auth' => \App\Http\Middleware\CronAuthenticate::class,
         'enforce.rules' => \App\Http\Middleware\EnforceRule::class,
-        'api.authentication' => \App\Http\Middleware\ApiAuthentication::class,
+        'api.auth' => \App\Http\Middleware\AuthenticateApi::class,
     ];
 
     /**
@@ -104,6 +104,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\EnsurePasswordIsNotExpired::class,
         \App\Http\Middleware\SelectTenant::class,
         \App\Http\Middleware\AuthorizeAnalytics::class,
-        \App\Http\Middleware\ApiAuthentication::class,
+        \App\Http\Middleware\AuthenticateApi::class,
     ];
 }
