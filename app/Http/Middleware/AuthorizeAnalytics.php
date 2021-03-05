886220abe9c9bc79827831d72ce51aaefee634ab
Davide Porrovecchio <pdavide@gmail.com>
2021-03-05 22:29:34 +0100

    Check for match between resource and scope

--- app/Http/Middleware/AuthorizeAnalytics.php
+++ app/Http/Middleware/AuthorizeAnalytics.php
@@ -21,29 +21,43 @@ class AuthorizeAnalytics
     public function handle($request, Closure $next, string $action)
     {
         $authUser = $request->user();
+
         if ($authUser->can($action)) {
             $authorized = true;
         }
 
         $currentPublicAdministration = current_public_administration();
+
         if ($request->route('website')) {
             if ($authUser->can($action, $request->route('website'))) {
                 $authorized = true;
             }
+
             if ($request->routeIs('websites.tracking.check') || $request->routeIs('websites.activate.force') || $request->routeIs('websites.snippet.javascript')) {
                 if ($authUser->pendingPublicAdministrations->where('id', $currentPublicAdministration->id)->isNotEmpty()) {
                     $authorized = true;
                 }
             }
+
             if ($currentPublicAdministration->id !== $request->route('website')->publicAdministration->id) {
-                $authorized = false;
+                $notFound = true;
             }
         }
 
         if ($request->route('user')) {
             if (!$request->route('user')->publicAdministrationsWithSuspended->contains($currentPublicAdministration)) {
-                $authorized = false;
+                $notFound = true;
             }
+        }
+
+        if ($request->route('credential')) {
+            if ($currentPublicAdministration->id !== $request->route('credential')->publicAdministration->id) {
+                $notFound = true;
+            }
+        }
+
+        if ($notFound ?? false) {
+            abort(404);
         }
 
         if (!($authorized ?? false)) {
