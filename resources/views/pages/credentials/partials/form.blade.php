8477d7aff91bd03612bc4a43ab715d8874c7b5b8
Davide Porrovecchio <pdavide@gmail.com>
2021-03-05 22:39:50 +0100

    Update and fix credential related views

--- resources/views/pages/credentials/partials/form.blade.php
+++ resources/views/pages/credentials/partials/form.blade.php
@@ -32,10 +32,6 @@
             <small id="name-input-help" class="form-text text-muted">{{ __('Scegli un nome per la credenziale.') }}</small>
         </div>
     </div>
-    <div class="form-row">
-        <h4 class="text-uppercase m-0">{{ __('PERMESSI SUI SITI WEB') }}</h4>
-
-    </div>
     <div class="form-row mt-5">
         <div class="form-group has-form-text col-md-7">
             <div class="input-group">
@@ -45,11 +41,11 @@
                 <div class="bootstrap-select-wrapper form-control flex-grow-1{{ $errors->has('type') ? ' is-invalid' : '' }}">
                     <label for="type">{{ ucfirst(__('tipologia')) }}</label>
                     <select title="{{ __('Scegli una tipologia') }}" id="type" name="type" aria-required="true" required>
-                        <option value="admin" {{ old('type', $type ?? '') === 'admin' ? 'selected' : '' }}>
-                            {{ __('Amministratore') }}
+                        <option value="{{ CredentialType::ADMIN }}" {{ old('type', $type ?? '') === CredentialType::ADMIN ? 'selected' : '' }}>
+                            {{ CredentialType::getDescription(CredentialType::ADMIN) }}
                         </option>
-                        <option value="analytics" {{ old('type', $type ?? '') === 'analytics' ? 'selected' : '' }}>
-                            {{ __('Analytics') }}
+                        <option value="{{ CredentialType::ANALYTICS }}" {{ old('type', $type ?? '') === CredentialType::ANALYTICS ? 'selected' : '' }}>
+                            {{ CredentialType::getDescription(CredentialType::ANALYTICS) }}
                         </option>
                     </select>
                 </div>
@@ -62,6 +58,36 @@
             <small id="type-input-help" class="form-text text-muted">
                 {{ __("L'amministratore può gestire tutti i siti web e tutti gli utenti") }}
             </small>
+        </div>
+    </div>
+    <h3 class="section-header{{ $errors->has('permissions') ? ' is-invalid' : '' }}">{{ __('Permessi sui siti web') }}</h3>
+    @error('permissions')
+    <div class="invalid-feedback">{{ $errors->first('permissions') }}</div>
+    @enderror
+    <h6>
+        {!! __('Per le credenziali di tipologia :analytics_credential, puoi assegnare permessi diversificati per ciascun sito.', [
+            'analytics_credential' => '<em>' . CredentialType::getDescription(CredentialType::ANALYTICS) . '</em>'
+        ]) !!}
+    </h6>
+    <div class="row justify-content-between">
+        <div class="col-md-6">
+            <small>
+                <ul>
+                    <li>{{ CredentialPermission::getLongDescription(CredentialPermission::READ) }}</li>
+                    <li>{{ CredentialPermission::getLongDescription(CredentialPermission::WRITE) }}</li>
+                </ul>
+            </small>
+        </div>
+        <div class="col-md-5">
+            <div class="callout callout-highlight note m-0">
+                <p class="font-italic">
+                    <small>
+                        {!! __('I permessi per le credenziali di tipologia :admin_credential non possono essere modificati.', [
+                            'admin_credential' => '<strong>' . CredentialType::getDescription(CredentialType::ADMIN) . '</strong>'
+                        ]) !!}
+                    </small>
+                </p>
+            </div>
         </div>
     </div>
     <div class="form-row">
