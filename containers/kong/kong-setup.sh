7f6272ee3a674266cfa603b7733930c7b481f87f
Davide Porrovecchio <pdavide@gmail.com>
2021-04-07 15:48:14 +0200

    Add configurable api version and basepath

--- containers/kong/kong-setup.sh
+++ containers/kong/kong-setup.sh
@@ -11,9 +11,9 @@ if ! curl -s http://localhost:8001/services | grep -q "nginx"; then
     curl --output /dev/null --silent -X POST http://localhost:8001/plugins/ --data "name=cors&config.origins=https://localhost"
     curl --output /dev/null --silent -X POST http://localhost:8001/plugins/ --data "name=oauth2&config.enable_client_credentials=true&config.global_credentials=true&config.accept_http_if_already_terminated=true"
     curl --output /dev/null --silent -X POST http://localhost:8001/services --data "name=portal&url=https://nginx/api"
-    curl --output /dev/null --silent -X POST http://localhost:8001/services/portal/routes --data "paths[]=/portal&name=portal"
+    curl --output /dev/null --silent -X POST http://localhost:8001/services/portal/routes --data "paths[]=/@KONG_API_PORTAL_BASE_PATH@&name=portal"
     curl --output /dev/null --silent -X POST http://localhost:8001/services --data "name=matomo&url=http://express:7080"
-    curl --output /dev/null --silent -X POST http://localhost:8001/services/matomo/routes --data "paths[]=/matomo&name=matomo"
+    curl --output /dev/null --silent -X POST http://localhost:8001/services/matomo/routes --data "paths[]=/@KONG_API_MATOMO_BASE_PATH@&name=matomo"
 fi
 
 echo "Kong plugins and routes are ready"
