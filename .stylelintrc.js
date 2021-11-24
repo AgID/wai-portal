16333a51fe4e9ff2aedd2246adc4fc008fac2419
Davide Porrovecchio <pdavide@gmail.com>
2021-11-24 18:29:08 +0100

    Update stylelint configuration

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   'rules': {
     'at-rule-no-unknown': [true, {
       'ignoreAtRules': ['each', 'extend', 'include', 'mixin', 'use']
-    }]
+    }],
+    'scss/at-extend-no-missing-placeholder': null
   },
-  'extends': 'stylelint-config-standard',
+  'extends': 'stylelint-config-standard-scss',
 }
